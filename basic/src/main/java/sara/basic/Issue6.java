b7faf68a209ac0182754b4be3629369456f80936
SayHello123 <sarachang757@gmail.com>
2020-05-29 16:01:12 +0800

    更改變數的值

--- basic/src/main/java/sara/basic/Issue6.java
+++ basic/src/main/java/sara/basic/Issue6.java
@@ -2,11 +2,12 @@ package sara.basic;
 
 public class Issue6 {
 	public static void main(String[] args) {
-		int t = 1;
-		int sinbay = 1;
-		int gaybay = 1;
+		int t = 0;
+		int sinbay = 0;
+		int gaybay = 0;
 		String result1, result2;
-		while(t < 99 && sinbay < 3 && gaybay < 3) {
+		
+		while(t < 100 && sinbay < 3 && gaybay < 3) {
 			if(Math.random() > 0.5) {
 				result1 = "陽"; 
 				sinbay++;
@@ -19,6 +20,5 @@ public class Issue6 {
 				System.out.println(gaybay + result2);
 			}
 		}
-		System.out.println("聖杯次數：" + sinbay +" "+ "無筊次數：" + gaybay);
-	}
+		System.out.println("聖杯：" + sinbay +" "+ "無筊：" + gaybay + " " + "擲筊次數：" + t); }
 }
