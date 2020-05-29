0216c109288c87077da662d7d25a4dbb2eb7e328
SayHello123 <sarachang757@gmail.com>
2020-05-29 16:24:40 +0800

    新增沒結論

--- basic/src/main/java/sara/basic/Issue6.java
+++ basic/src/main/java/sara/basic/Issue6.java
@@ -5,20 +5,25 @@ public class Issue6 {
 		int t = 0;
 		int sinbay = 0;
 		int gaybay = 0;
+		int noend = 0;
 		String result1, result2;
 		
-		while(t < 100 && sinbay < 3 && gaybay < 3) {
+		while(noend < 100 && sinbay < 3 && gaybay < 3) {
 			if(Math.random() > 0.5) {
 				result1 = "陽"; 
 				sinbay++;
-				t++;
 				System.out.println(sinbay + result1);
 			}else {
 				result2 = "陰";
 				gaybay++;
-				t++;
 				System.out.println(gaybay + result2);
+				
+				if (sinbay < 2 && gaybay < 2) {
+					noend++;
+				}
 			}
 		}
-		System.out.println("聖杯：" + sinbay +" "+ "無筊：" + gaybay + " " + "擲筊次數：" + t); }
+		t = sinbay + gaybay;
+		System.out.println("聖杯：" + sinbay +" "+ "無筊：" + gaybay + " " + "沒結論：" + " " + noend + " " + "擲筊次數：" + t); 
+		}
 }
