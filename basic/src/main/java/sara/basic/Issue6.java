1fcaf86bdd88f8555fb1ab2a2c4e7e7b88306841
SayHello123 <sarachang757@gmail.com>
2020-06-05 18:01:36 +0800

    修正縮排、刪除「陰、陽」、加入 boolean

--- basic/src/main/java/sara/basic/Issue6.java
+++ basic/src/main/java/sara/basic/Issue6.java
@@ -2,30 +2,22 @@ package sara.basic;
 
 public class Issue6 {
 	public static void main(String[] args) {
-		int bay1, bay2;
-		String result;
-		
-		if(Math.random() > 0.5) {
-			bay1 = 20;
-			System.out.println("陽");
-		}else {
-			bay1 = 10;
-			System.out.println("陰");
+		boolean bay1 = Math.random() > 0.5;
+		boolean bay2 = Math.random() > 0.5;
+		String result = null;
+
+		if(bay1 == true && bay2 == true) {
+			result = "無筊";
 		}		
-			if(Math.random() > 0.5) {
-				bay2 = 20;
-				System.out.println("陽");
-			}else {
-				bay2 = 10;
-				System.out.println("陰");
-			}
-				if(bay1 + bay2 >= 40) {
-					result = "無筊";
-				}else if (bay1 + bay2 >= 30) {
-					result = "聖筊";
-				}else {
-					result = "笑筊";
-				}
+		if(bay1 == false && bay2 == false) {
+			result = "笑筊";
+		}
+		if(bay1 == true && bay2 == false) {
+			result = "聖筊";
+		}
+		if(bay1 == false && bay2 == true) {
+			result = "聖筊";
+		}
 		System.out.println(result);
 	}
-}
+} 
