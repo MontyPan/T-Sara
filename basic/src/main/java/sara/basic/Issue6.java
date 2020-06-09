3f98f9da60bdfd8fcf4ca30c072ba2989f9df7c2
SayHello123 <sarachang757@gmail.com>
2020-06-09 20:36:51 +0800

    加上 while 的成品

--- basic/src/main/java/sara/basic/Issue6.java
+++ basic/src/main/java/sara/basic/Issue6.java
@@ -4,15 +4,37 @@ public class Issue6 {
 	public static void main(String[] args) {
 		boolean bay1 = Math.random() > 0.5;
 		boolean bay2 = Math.random() > 0.5;
+		int sinBay = 0;
+		int noBay = 0;
+		int noResult = 0;
+		int baySum = 0;
 		String result;
 
-		if(bay1 && bay2) {
-			result = "無筊";
-		}else if(bay1 != true && bay2 != true) {
-			result = "笑筊";
-		}else  {
-			result = "聖筊";
+		while(noResult < 100 && sinBay < 3 && noBay < 3) {
+			if(bay1 && bay2) {
+				noBay++;
+				noResult++;
+				baySum++;
+				sinBay = 0;
+			}else if(bay1 != true && bay2 != true) {
+				noResult++;
+				baySum++;
+				noBay = 0;
+				sinBay = 0;
+			}else  {
+				sinBay++;
+				noResult++;
+				baySum++;
+				noBay = 0;
+			}
 		}
-		System.out.println(result);
+		if(noBay >= 3) {
+			result = "無筊";
+		}else if(sinBay >= 3) {
+			result = "聖筊";
+		}else {
+			result = "沒結論";
+		}
+		System.out.println("擲筊結果：" + " " + result + " " + "==>" + " " + "擲筊次數：" + " " + baySum);
 	}
 } 
