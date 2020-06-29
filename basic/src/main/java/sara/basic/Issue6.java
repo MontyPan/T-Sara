053f6ba3b8d311bb8ce161ef3abdc42fb9f7078a
SayHello123 <sarachang757@gmail.com>
2020-06-29 16:46:02 +0800

    簡化程式碼 Ver.3

--- basic/src/main/java/sara/basic/Issue6.java
+++ basic/src/main/java/sara/basic/Issue6.java
@@ -3,22 +3,18 @@ package sara.basic;
 public class Issue6 {
 	public static void main(String[] args) {
 		//分成兩個杯並賦予其值，true 表示筊的凸面；反之， false 表示筊的平面
-		boolean bay1 = Math.random() > 0.5;
-		boolean bay2 = Math.random() > 0.5;
-		int sinBaySum = 0; //用於計算擲出聖筊是否連續三次
-		int noBaySum = 0;  //用於計算擲出無筊是否連續三次
-		int baySum = 0;    //用於計算總擲筊次數
+		boolean bay1 = Math.random() > 0.5, bay2 = Math.random() > 0.5;
+		int sinBaySum = 0, noBaySum = 0, baySum = 0;
 		String result;
 		
-		//當 noResult<100、sinBaySum<3、noBaySum<3，任一條件不符合時，就會停止迴圈
 		while(baySum < 100 && sinBaySum < 3 && noBaySum < 3) {
 			baySum++;
 			if(bay1 && bay2) {  //當兩個筊都是凸面時，就執行以下內容
 				noBaySum++;  //無筊連續次數 +1
 				sinBaySum = 0;  //不是聖筊，所以歸零
 			}else if(!bay1 && !bay2) {  //當兩個筊都是平面，就執行以下內容
-				noBaySum = 0;  //不是無筊，所以歸零
-				sinBaySum = 0; //不是聖筊，所以歸零
+				noBaySum = 0;
+				sinBaySum = 0;
 			}else {  //當兩個筊為一凸一平，就執行以下內容
 				sinBaySum++;  //聖筊連續次數 +1
 				noBaySum = 0; //不是無筊，所以歸零
