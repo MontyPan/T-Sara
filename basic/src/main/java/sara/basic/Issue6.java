aa5e6b32a8a15f5f41125e15cd8e016f412b7763
SayHello123 <sarachang757@gmail.com>
2020-07-02 23:07:07 +0800

    簡化程式碼 Ver.5

--- basic/src/main/java/sara/basic/Issue6.java
+++ basic/src/main/java/sara/basic/Issue6.java
@@ -3,23 +3,24 @@ package sara.basic;
 public class Issue6 {
 	public static void main(String[] args) {
 		//分成兩個杯並賦予其值，true 表示筊的凸面；反之， false 表示筊的平面
-		boolean bay1 = Math.random() > 0.5, bay2 = Math.random() > 0.5;
+		boolean bay1, bay2;
 		int sinBaySum = 0, noBaySum = 0, baySum = 0;
 		String result;
 		
 		while(baySum < 100 && sinBaySum < 3 && noBaySum < 3) {
+			bay1 = Math.random() > 0.5; //擲筊
+			bay2 = Math.random() > 0.5; //擲筊
 			baySum++;
 			if(bay1 && bay2) {  //當兩個筊都是凸面時，就執行以下內容
 				noBaySum++;  //無筊連續次數 +1
 				sinBaySum = 0;  //不是聖筊，所以歸零
 			}else if(!bay1 && !bay2) {  //當兩個筊都是平面，就執行以下內容
-				noBaySum = sinBaySum = 0; //不是聖筊和無筊，都歸零
+				noBaySum = 0;  //不是無筊，所以歸零
+				sinBaySum = 0; //不是聖筊，所以歸零
 			}else {  //當兩個筊為一凸一平，就執行以下內容
 				sinBaySum++;  //聖筊連續次數 +1
 				noBaySum = 0; //不是無筊，所以歸零
 			}
-			bay1 = Math.random() > 0.5; //擲新的筊
-			bay2 = Math.random() > 0.5; //擲新的筊
 		}
 		 if(noBaySum == 3) {
 			 result = "無筊";
