f211621c31de242fc6edb8c08520f29642d449c4
SayHello123 <sarachang757@gmail.com>
2020-09-11 07:37:52 +0800

    簡化程式碼V2

--- basic/src/main/java/sara/basic/Issue15.java
+++ basic/src/main/java/sara/basic/Issue15.java
@@ -2,22 +2,21 @@ package sara.basic;
 
 public class Issue15 {
 	public static void main(String[] args) {
-		int a1 = 1; // 首項 
+		int a1 = 1; // 首項 = 1
 		int d = 2; // 公差 = 2
 		int n = 5; // 總項數 = 5
 		int x = 0; // 各項的值
 		int y = 0; // 項數計數器
-		int sum = 0; //計算等差數列之和
 		/*印出；1, 3, 5, 7, 9
 		25*/
-		while(y < n-1) {
+		while (y < n-1) {
 			x = a1 + (y * d);
 			y++;
 			System.out.print(x + ",");
 		}
 		System.out.println(x = a1 + (y * d));
-		sum = (a1 + (a1 + (n-1) * d)) * n / 2;
-		System.out.println(sum);
+		x = (a1 + (a1 + (n-1) * d)) * n / 2;
+		System.out.println(x);
 	}
 }
 	
