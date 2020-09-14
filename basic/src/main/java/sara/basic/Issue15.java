4e9bf1efa92192c1668e5448b63c9f56dcc73518
SayHello123 <sarachang757@gmail.com>
2020-09-14 21:11:21 +0800

    簡化程式碼V3

--- basic/src/main/java/sara/basic/Issue15.java
+++ basic/src/main/java/sara/basic/Issue15.java
@@ -7,16 +7,33 @@ public class Issue15 {
 		int n = 5; // 總項數 = 5
 		int x = 0; // 各項的值
 		int y = 0; // 項數計數器
-		/*印出；1, 3, 5, 7, 9
-		25*/
-		while (y < n-1) {
+		int sum = 0; // 計算總和
+		/*
+		 * 印出；1, 3, 5, 7, 9 
+		 * 25
+		 */
+		while (y < n) {
 			x = a1 + (y * d);
+			if (y < n - 1) {
+				System.out.print(x + ", ");
+			} else if (y < n) {
+				System.out.println(x);
+				sum = (a1 + (a1 + (n - 1) * d)) * n / 2;
+				System.out.println(sum);
+			}
 			y++;
-			System.out.print(x + ",");
 		}
-		System.out.println(x = a1 + (y * d));
-		x = (a1 + (a1 + (n-1) * d)) * n / 2;
-		System.out.println(x);
+//		while (y < n-1) {
+//			x = a1 + (y * d);
+//			y++;
+//			System.out.print(x + ",");
+//		}
+//		while (y < n) {
+//			x = a1 + (y * d);
+//			y++;
+//			System.out.print(x);
+//		}
+//		sum = (a1 + (a1 + (n-1) * d)) * n / 2;
+//		System.out.println(sum);
 	}
 }
-	
