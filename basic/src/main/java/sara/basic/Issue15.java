9ec4f467e67b2c6ace94802664793fe8744ef4a4
SayHello123 <sarachang757@gmail.com>
2020-09-16 21:54:21 +0800

    簡化程式碼V4

--- basic/src/main/java/sara/basic/Issue15.java
+++ basic/src/main/java/sara/basic/Issue15.java
@@ -2,38 +2,17 @@ package sara.basic;
 
 public class Issue15 {
 	public static void main(String[] args) {
-		int a1 = 1; // 首項 = 1
+		int a1 = 1; // 首項 
 		int d = 2; // 公差 = 2
 		int n = 5; // 總項數 = 5
-		int x = 0; // 各項的值
 		int y = 0; // 項數計數器
-		int sum = 0; // 計算總和
-		/*
-		 * 印出；1, 3, 5, 7, 9 
-		 * 25
-		 */
-		while (y < n) {
-			x = a1 + (y * d);
-			if (y < n - 1) {
-				System.out.print(x + ", ");
-			} else if (y < n) {
-				System.out.println(x);
-				sum = (a1 + (a1 + (n - 1) * d)) * n / 2;
-				System.out.println(sum);
-			}
+		/*印出；1, 3, 5, 7, 9
+		25*/
+		while (y < n-1) {
+			System.out.print(a1 + (y * d) + ",");
 			y++;
 		}
-//		while (y < n-1) {
-//			x = a1 + (y * d);
-//			y++;
-//			System.out.print(x + ",");
-//		}
-//		while (y < n) {
-//			x = a1 + (y * d);
-//			y++;
-//			System.out.print(x);
-//		}
-//		sum = (a1 + (a1 + (n-1) * d)) * n / 2;
-//		System.out.println(sum);
+		System.out.println(a1 + (y * d));
+		System.out.println((a1 + (a1 + (n-1) * d)) * n / 2);
 	}
 }
