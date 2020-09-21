3a9a160e14fbbc46c7c397e0e885e80f7a54801d
SayHello123 <sarachang757@gmail.com>
2020-09-21 19:58:15 +0800

    Issue 18V3

--- basic/src/main/java/sara/basic/Issue18.java
+++ basic/src/main/java/sara/basic/Issue18.java
@@ -5,12 +5,10 @@ public class Issue18 {
 		int a1 = 1; // 首項 
 		int d = 2; // 公差 = 2
 		int n = 5; // 總項數 = 5
-		int y = 0; // 項數計數器
 		/*印出；1, 3, 5, 7, 9
 		25*/
 		System.out.print(a1);
-		for (int i = 0; i < n-1; i++) {			
-			y++;
+		for (int y = 1; y < n; y++) {			
 			System.out.print("," + (a1 + (y * d)) );
 		}
 		System.out.println("\n" + (a1 + (a1 + (n-1) * d)) * n / 2);
