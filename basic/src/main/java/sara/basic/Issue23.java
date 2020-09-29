3079a4eeab638781227b1614df001877b80b8f29
SayHello123 <sarachang757@gmail.com>
2020-09-29 19:11:21 +0800

    Add Issue 23V1

---
+++ basic/src/main/java/sara/basic/Issue23.java
@@ -0,0 +1,33 @@
+package sara.basic;
+
+public class Issue23 {
+
+	public static void main(String[] args) {
+		int wtf = 5;
+		part0(wtf);
+		System.out.println("\n====================");
+		part1(wtf);
+		System.out.println("====================");
+		part2(wtf);
+		
+	}
+	
+	private static void part0(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.print("*");
+		}
+	}
+	
+	private static void part1(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println("*");
+		}
+	}
+	
+	private static void part2(int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println("*****");
+		}
+	}
+
+}
