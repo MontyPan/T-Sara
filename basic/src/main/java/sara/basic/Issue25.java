d3cb0e3057f9f32bbef596e45408b1ec7424e312
SayHello123 <sarachang757@gmail.com>
2020-10-29 06:40:58 +0800

    update part2

--- basic/src/main/java/sara/basic/Issue25.java
+++ basic/src/main/java/sara/basic/Issue25.java
@@ -25,21 +25,31 @@ public class Issue25 {
 			System.out.println();
 		}
 	}
-	
+
 	private static void part2(int n) {
 		for (int i = 0; i < n; i++) {
 			for (int i2 = 0; i2 <= i; i2++) {
 				System.out.print("*");
 			}
-			for (int i2 = 0; i2 <= 6-(i*2); i2++) {
+			for (int i2 = n - 1; i2 > i; i2--) {
+				System.out.print(" ");
+			}
+			for (int i2 = n - 1; i2 > i + 1; i2--) {
 				System.out.print(" ");
 			}
 			for (int i2 = 0; i2 <= i; i2++) {
 				if (i2 < 4) {
 					System.out.print("*");
-				}else {
-					System.out.print("");
 				}
+			}
+			System.out.println();
+		}
+	}
+
+	private static void part3(int n) {
+		for (int i = 1; i < n; i++) {
+			for (int i2 = n; i2 > i; i2--) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
