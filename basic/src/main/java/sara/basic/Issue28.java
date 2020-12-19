6f8ea26d68100c76cdd7d9cbb3282be4899ddb01
SayHello123 <sarachang757@gmail.com>
2020-12-19 18:50:37 +0800

    create Issue-28, add players number judgement

---
+++ basic/src/main/java/sara/basic/Issue28.java
@@ -0,0 +1,27 @@
+package sara.basic;
+
+import java.util.Scanner;
+
+public class Issue28 {
+
+	public static void main(String[] args) {
+		// 輸入玩家人數，二人玩一人發 12 張，三人玩一人發 8 張，四人玩一人發 6 張
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入遊戲人數：2~4");
+		int playersSum = sc.nextInt();
+		if (playersSum == 2) {
+			int[][] players = new int[2][13];
+			System.out.println("兩人遊戲開始~");
+		} else if (playersSum == 3) {
+			int[][] players = new int[3][8];
+			System.out.println("三人遊戲開始~");
+		} else if (playersSum == 4) {
+			int[][] players = new int[4][6];
+			System.out.println("四人遊戲開始~");
+		}
+		
+		
+		
+		
+	}
+}
