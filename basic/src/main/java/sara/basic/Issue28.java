3a2cc3b32eabfabf1229c2057735236af678ccc0
SayHello123 <sarachang757@gmail.com>
2020-12-19 19:11:40 +0800

    update players number judgement

--- basic/src/main/java/sara/basic/Issue28.java
+++ basic/src/main/java/sara/basic/Issue28.java
@@ -5,12 +5,18 @@ import java.util.Scanner;
 public class Issue28 {
 
 	public static void main(String[] args) {
+
 		// 輸入玩家人數，二人玩一人發 12 張，三人玩一人發 8 張，四人玩一人發 6 張
 		Scanner sc = new Scanner(System.in);
 		System.out.println("請輸入遊戲人數：2~4");
 		int playersSum = sc.nextInt();
+		while (playersSum <= 1 || playersSum >= 5) {
+			System.out.println("請重新輸入人數");
+			playersSum = sc.nextInt();
+		}
+
 		if (playersSum == 2) {
-			int[][] players = new int[2][13];
+			int[][] players = new int[2][12];
 			System.out.println("兩人遊戲開始~");
 		} else if (playersSum == 3) {
 			int[][] players = new int[3][8];
@@ -19,9 +25,6 @@ public class Issue28 {
 			int[][] players = new int[4][6];
 			System.out.println("四人遊戲開始~");
 		}
-		
-		
-		
-		
+
 	}
 }
