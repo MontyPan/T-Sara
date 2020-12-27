71016f90aa09dd456fe423bf1476567d245faba4
SayHello123 <sarachang757@gmail.com>
2020-12-28 01:23:05 +0800

    add second compare & separate main into methods

--- basic/src/main/java/sara/basic/pickred/Issue28.java
+++ basic/src/main/java/sara/basic/pickred/Issue28.java
@@ -7,43 +7,45 @@ import java.util.Scanner;
 
 public class Issue28 {
 
+	static String[][] players;
+	static String[] showedPoker;
+	static String[] shuffledPoker;
+	static int[] score;
+	static int diffScore;
+	static boolean isMatch;
+
 	public static void main(String[] args) {
+// 輸入玩家人數
+		int playersSum = getPlayersSum();
 
-		// 輸入玩家人數
-		Scanner sc = new Scanner(System.in);
-		System.out.println("請輸入遊戲人數：2~4");
-		int playersSum = sc.nextInt();
-		while (playersSum <= 1 || playersSum >= 5) {
-			System.out.println("請重新輸入人數");
-			playersSum = sc.nextInt();
-		}
+// 總共 4 花色 A~K = 52 張牌
+// 黑桃、梅花(不計分)、紅心(紅)、方塊(紅)
 
-		// 總共 4 花色 A~K = 52 張牌
-		// 黑桃、梅花(不計分)、紅心(紅)、方塊(紅)
-
-		// 生成花色 & 牌
+// 生成花色 & 牌
 		String[] suits = { "♥", "♦", "♣", "♠" };
 		int[] cards = new int[13];
 		String[][] temp = new String[4][13];
 		String[] poker = new String[52];
-		// 把每張牌加入花色
+
+// 把每張牌加入花色
 		for (int i = 0; i < suits.length; i++) {
 			for (int i2 = 0; i2 < cards.length; i2++) {
 				cards[i2] = i2 + 1;
 				temp[i][i2] = suits[i] + cards[i2];
 			}
 		}
-		// 把加入花色的牌放入 poker 中
+
+// 把加入花色的牌放入 poker 中
 		for (int i = 0; i < poker.length; i++) {
 			poker[i] = temp[i % 4][i / 4];
 		}
 
-		// 洗牌
-		String[] shuffledPoker = new String[52];
+// 洗牌
+		shuffledPoker = new String[52];
 		for (int i = 0; i < shuffledPoker.length; i++) {
 			int rand;
 			boolean isSame;
-			// 避免重複
+// 避免重複
 			do {
 				rand = (int) (Math.random() * 52);
 				isSame = false;
@@ -54,54 +56,20 @@ public class Issue28 {
 					}
 				}
 			} while (isSame);
-
 			shuffledPoker[i] = poker[rand];
 		}
-		// 將字串統一長度
+
+// 將字串統一長度
 		for (int i = 0; i < shuffledPoker.length; i++) {
 			if (shuffledPoker[i].length() <= 2) {
 				shuffledPoker[i] += " ";
 			}
 		}
 
-		// 依照玩家人數發牌，二人玩一人發 12 張，三人玩一人發 8 張，四人玩一人發 6 張
-		String[][] players = null;
-		if (playersSum == 2) {
-			System.out.println("兩人遊戲開始~");
-			players = new String[2][12];
-			for (int i2 = 0; i2 < players[0].length; i2++) {
-				players[0][i2] = shuffledPoker[i2];
-				players[1][i2] = shuffledPoker[i2 + (1 * players[1].length)];
-				shuffledPoker[i2] = "0";
-				shuffledPoker[i2 + (1 * players[1].length)] = "0";
-			}
-		} else if (playersSum == 3) {
-			System.out.println("三人遊戲開始~");
-			players = new String[3][8];
-			for (int i2 = 0; i2 < players[0].length; i2++) {
-				players[0][i2] = shuffledPoker[i2];
-				players[1][i2] = shuffledPoker[i2 + (1 * players[1].length)];
-				players[2][i2] = shuffledPoker[i2 + (2 * players[2].length)];
-				shuffledPoker[i2] = "0";
-				shuffledPoker[i2 + (1 * players[1].length)] = "0";
-				shuffledPoker[i2 + (2 * players[2].length)] = "0";
-			}
-		} else if (playersSum == 4) {
-			System.out.println("四人遊戲開始~");
-			players = new String[4][6];
-			for (int i2 = 0; i2 < players[0].length; i2++) {
-				players[0][i2] = shuffledPoker[i2];
-				players[1][i2] = shuffledPoker[i2 + (1 * players[1].length)];
-				players[2][i2] = shuffledPoker[i2 + (2 * players[2].length)];
-				players[3][i2] = shuffledPoker[i2 + (3 * players[3].length)];
-				shuffledPoker[i2] = "0";
-				shuffledPoker[i2 + (1 * players[1].length)] = "0";
-				shuffledPoker[i2 + (2 * players[2].length)] = "0";
-				shuffledPoker[i2 + (3 * players[3].length)] = "0";
-			}
-		}
+// 依照玩家人數發牌，二人玩一人發 12 張，三人玩一人發 8 張，四人玩一人發 6 張
+		players = dealCardToEachPlayer(playersSum, shuffledPoker);
 
-		// 每個玩家擁有的手排
+// 每個玩家擁有的手牌
 		for (String[] player : players) {
 			for (String card : player) {
 				System.out.print(card + ",");
@@ -109,166 +77,219 @@ public class Issue28 {
 			System.out.println();
 		}
 
-		// 把發出去的牌的空間與元素移除
-		List<String> tempShuffledList = new ArrayList<String>(Arrays.asList(shuffledPoker));
-		for (int i = 0; i < shuffledPoker.length; i++) {
-			if (shuffledPoker[i] == "0") {
-				tempShuffledList.remove("0");
-			}
-		}
-		shuffledPoker = tempShuffledList.toArray(new String[tempShuffledList.size()]);
-		// 翻出剩餘的牌前四張於桌面
-		// 海底翻開的牌
-		String[] showedPoker = new String[4];
+// 把發出去的牌的空間與元素移除
+		shuffledPoker = removeEmptyElement(shuffledPoker);
+
+// 翻出剩餘的牌前四張於桌面
+// 海底翻開的牌
+		showedPoker = new String[4];
 		for (int i = 0; i < 4; i++) {
 			showedPoker[i] = shuffledPoker[i];
 			shuffledPoker[i] = "0";
-			System.out.print("海底牌：" + showedPoker[i]);
 		}
-		System.out.println();
-		// 把翻出的牌的空間與元素移除
-		tempShuffledList = new ArrayList<String>(Arrays.asList(shuffledPoker));
-		for (int i = 0; i < shuffledPoker.length; i++) {
-			if (shuffledPoker[i] == "0") {
-				tempShuffledList.remove("0");
+		System.out.println("海底的牌：" + Arrays.toString(showedPoker));
+
+// 把翻出的牌的空間與元素移除
+		shuffledPoker = removeEmptyElement(shuffledPoker);
+		System.out.println("洗牌完成，開始遊戲\n蓋牌：" + Arrays.toString(shuffledPoker));
+
+// 遊戲開始：輪流打出手牌
+// 計分板
+		score = new int[players.length];
+
+		gamePlaying();
+		System.out.println("遊戲結束");
+		int total = 0;
+		for (int i = 0; i < players.length; i++) {
+			System.out.println("玩家 " + i + " 剩餘手牌：" + players[i].length + "、分數為：" + score[i]);
+			total += score[i];
+		}
+		System.out.println("總分：" + total);
+		int winner = score[0];
+		int winPlayer = 0;
+		for (int i = 1; i < score.length; i++) {
+			if (score[i] > winner) {
+				winner = score[i];
+				winPlayer = i;
 			}
 		}
-		shuffledPoker = tempShuffledList.toArray(new String[tempShuffledList.size()]);
-		System.out.println("第一次洗牌\n" + Arrays.toString(shuffledPoker));
-		// 遊戲開始：輪流打出手牌
-		// 計分板
-		int[] score = new int[players.length];
+		System.out.println("玩家" + winPlayer + "贏了，分數為：" + winner);
+	}
 
-		for (int i = 0; i < showedPoker.length; i++) {
-			for (int i2 = 0; i2 < players.length; i2++) {
-				for (int i3 = 0; i3 < players[i2].length; i3++) {
-					List<String> tempShowedList;
-					List<String> tempPlayerPokerList;
-					int numberFromPlayer = Integer.parseInt(players[i2][i3].substring(1).trim());
-					int numberFromShowed = Integer.parseInt(players[i2][i3].substring(1).trim());
-
-					String suitsFromPlayer = players[i2][i3].substring(0, 1);
-					String suitsFromShowed = showedPoker[i].substring(0, 1);
-
-					if ((numberFromPlayer + numberFromShowed) == 10) {
-						System.out.printf("第一玩家%d：%d與海底的%d配對\n", i2, numberFromPlayer, numberFromShowed);
-						// 判斷計分
-						if (suitsFromPlayer.equals("♥") || suitsFromPlayer.equals("♦")) {
-							score[i2] += numberFromPlayer;
-						}
-						if (suitsFromShowed.equals("♥") || suitsFromShowed.equals("♦")) {
-							score[i2] += numberFromShowed;
-						}
-						if (players[i2][i3].trim().equals("♠1") || showedPoker[i].trim().equals("♠1")) {
-							score[i2] += 30;
-						}
-						if (players[i2][i3].trim().equals("♥1") || showedPoker[i].trim().equals("♥1")) {
-							score[i2] += 20;
-						}
-						if (players[i2][i3].trim().equals("♦1") || showedPoker[i].trim().equals("♦1")) {
-							score[i2] += 20;
-						}
-						// 把配對成功的兩張牌丟掉
-						// 翻出一張牌
-						players[i2][i3] = "0";
-						tempPlayerPokerList = new ArrayList<String>(Arrays.asList(players[i2]));
-						tempPlayerPokerList.remove("0");
-						players[i2] = tempPlayerPokerList.toArray(new String[tempPlayerPokerList.size()]);
-						System.out.println("玩家" + i2 + "的手牌： " + Arrays.toString(players[i2]));
-						showedPoker[i] = "0";
-						tempShowedList = new ArrayList<String>(Arrays.asList(showedPoker));
-						tempShowedList.remove("0");
-						tempShowedList.add(shuffledPoker[0]);
-
-						showedPoker = tempShowedList.toArray(new String[tempShowedList.size()]);
-
-						System.out.println("海底： " + Arrays.toString(showedPoker));
-						shuffledPoker[0] = "0";
-						tempShuffledList = new ArrayList<String>(Arrays.asList(shuffledPoker));
-						tempShuffledList.remove("0");
-						shuffledPoker = tempShuffledList.toArray(new String[tempShuffledList.size()]);
-						System.out.println("配對後洗牌： " + Arrays.toString(shuffledPoker));
-						// 判斷翻出的這張牌是否可跟手牌湊
-						// 可以：把配對成功的兩張牌丟掉
-						// 判斷計分 - 誰得幾分
-						// 不行：放置海底
-						// 換下一位
-						break;
-					} else if (numberFromPlayer >= 9 || numberFromShowed >= 9) {
-						if (numberFromPlayer == numberFromShowed) {
-							// 判斷計分
-							if (suitsFromPlayer.equals("♥") || suitsFromPlayer.equals("♦")) {
-								score[i2] += 10;
-								System.out.printf("第二玩家%d：%d與海底的%d配對\n", i2, numberFromPlayer, numberFromShowed);
-							}
-							if (suitsFromShowed.equals("♥") || suitsFromShowed.equals("♦")) {
-								score[i2] += 10;
-								System.out.printf("第三玩家%d：%d與海底的%d配對\n", i2, numberFromPlayer, numberFromShowed);
-							}
-							// 把配對成功的兩張牌丟掉
-							// 翻出一張牌
-							players[i2][i3] = "0";
-							tempPlayerPokerList = new ArrayList<String>(Arrays.asList(players[i2]));
-							tempPlayerPokerList.remove("0");
-							players[i2] = tempPlayerPokerList.toArray(new String[tempPlayerPokerList.size()]);
-							System.out.println("玩家" + i2 + "的手牌： " + Arrays.toString(players[i2]));
-							showedPoker[i] = "0";
-							tempShowedList = new ArrayList<String>(Arrays.asList(showedPoker));
-							tempShowedList.remove("0");
-							tempShowedList.add(shuffledPoker[0]);
-							showedPoker = tempShowedList.toArray(new String[tempShowedList.size()]);
-							System.out.println("海底： " + Arrays.toString(showedPoker));
-
-							shuffledPoker[0] = "0";
-							tempShuffledList = new ArrayList<String>(Arrays.asList(shuffledPoker));
-							tempShuffledList.remove("0");
-							shuffledPoker = tempShuffledList.toArray(new String[tempShuffledList.size()]);
-							System.out.println("配對後洗牌： " + Arrays.toString(shuffledPoker));
-							// 判斷這張是否可跟手牌湊
-							// 可以：把配對成功的兩張牌丟掉
-							// 判斷計分 - 誰得幾分
-							// 不行：放置海底
-							// 換下一位
+	public static void gamePlaying() {
+		while (showedPoker.length > 0 && shuffledPoker.length > 0) {
+			for (int i = 0; i < players.length; i++) {
+				for (int i2 = 0; i2 < players[i].length; i2++) {
+					isMatch = false;
+					for (int i3 = 0; i3 < showedPoker.length; i3++) {
+						isMatch = false;
+						isMatch = checkPairAndPoint(players[i][i2], showedPoker[i3], i);
+						if (isMatch) {
+							players[i][i2] = "0";
+							showedPoker[i3] = "0";
+							players[i] = removeEmptyElement(players[i]);
+							showedPoker = removeEmptyElement(showedPoker);
+// 配對成功，翻一張牌比對海底
+							pickAndMatchCard(i);
 							break;
 						}
-					} else {
-						// (玩家手中"沒牌"可湊)
-						// 打出一張手牌到海底
-						System.out.printf("第四玩家%d：沒牌出\n", i2);
-						String toShowedPoker = players[i2][i3];
-						players[i2][i3] = "0";
-						tempPlayerPokerList = new ArrayList<String>(Arrays.asList(players[i2]));
-						tempPlayerPokerList.remove("0");
-						players[i2] = tempPlayerPokerList.toArray(new String[tempPlayerPokerList.size()]);
-						System.out.println("玩家" + i2 + "的手牌： " + Arrays.toString(players[i2]));
-						// 翻出一張牌
-						tempShowedList = new ArrayList<String>(Arrays.asList(showedPoker));
-						tempShowedList.add(toShowedPoker);
-						tempShowedList.add(shuffledPoker[0]);
-						showedPoker = tempShowedList.toArray(new String[tempShowedList.size()]);
-						System.out.println("海底： " + Arrays.toString(showedPoker));
-						shuffledPoker[0] = "0";
-						tempShuffledList = new ArrayList<String>(Arrays.asList(shuffledPoker));
-						tempShuffledList.remove("0");
-						shuffledPoker = tempShuffledList.toArray(new String[tempShuffledList.size()]);
-						System.out.println("配對後洗牌： " + Arrays.toString(shuffledPoker));
-						// 判斷這張是否可跟手牌湊
-						// 可以：把配對成功的兩張牌丟掉
-						// 判斷計分 - 誰得幾分
-						// 不行：放置海底
-						// 換下一位
+					}
+					if (isMatch) {
 						break;
 					}
 				}
+				if (!isMatch) {
+					moveCardToShowed(i);
+					pickAndMatchCard(i);
+				} else if (showedPoker.length == 0 && shuffledPoker.length > 0) {
+					addCardToShowed();
+				}
 			}
 		}
-						// =遊戲結束條件=
-						// if 所有海底牌都被取完 && 所有玩家手牌都打出
-						// 分數高的獲勝
-						// =計分條件=
-						// 黑桃 A = 30
-						// 紅心 A ,方塊A = 20
-						// 紅色 9, 10, J, Q, K = 10
-						// 紅色 2~8 分數與牌面數字一樣
+	}
+
+	public static void addCardToShowed() {
+		showedPoker = addElement(showedPoker, shuffledPoker[0]);
+		shuffledPoker[0] = "0";
+		shuffledPoker = removeEmptyElement(shuffledPoker);
+	}
+
+	public static void moveCardToShowed(int player) {
+		if (players[player].length > 0) {
+			showedPoker = addElement(showedPoker, players[player][0]);
+			players[player][0] = "0";
+			players[player] = removeEmptyElement(players[player]);
+		}
+	}
+
+	public static void pickAndMatchCard(int player) {
+		if (shuffledPoker.length > 0) {
+// 翻牌堆第一張牌跟海底比對
+			boolean isSecondMatch = false;
+			for (int i = 0; i < showedPoker.length; i++) {
+				isSecondMatch = checkPairAndPoint(shuffledPoker[0], showedPoker[i], player);
+				if (isSecondMatch) {
+					shuffledPoker[0] = "0";
+					showedPoker[i] = "0";
+					shuffledPoker = removeEmptyElement(shuffledPoker);
+					showedPoker = removeEmptyElement(showedPoker);
+					break;
+				}
+			}
+			if (!isSecondMatch) {
+				showedPoker = addElement(showedPoker, shuffledPoker[0]);
+				shuffledPoker[0] = "0";
+				shuffledPoker = removeEmptyElement(shuffledPoker);
+			}
+		}
+	}
+
+	public static boolean checkPairAndPoint(String cardOne, String cardTwo, int player) {
+		int numberCardOne = Integer.parseInt(cardOne.substring(1).trim());
+		int numberCardTwo = Integer.parseInt(cardTwo.substring(1).trim());
+
+		if ((numberCardOne + numberCardTwo) == 10) {
+			countScore(cardOne, cardTwo, player);
+			return true;
+		} else if (numberCardOne > 9 && numberCardTwo > 9) {
+			if (numberCardOne == numberCardTwo) {
+				countScore(cardOne, cardTwo, player);
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static int getPlayersSum() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("請輸入遊戲人數：2~4");
+		int playersSum = sc.nextInt();
+		while (playersSum <= 1 || playersSum >= 5) {
+			System.out.println("請重新輸入人數");
+			playersSum = sc.nextInt();
+		}
+		return playersSum;
+	}
+
+	public static String[][] dealCardToEachPlayer(int playersSum, String[] shuffledPoker) {
+		if (playersSum == 2) {
+			System.out.println("玩家共兩位");
+			players = new String[2][12];
+		} else if (playersSum == 3) {
+			System.out.println("玩家共三位");
+			players = new String[3][8];
+		} else if (playersSum == 4) {
+			System.out.println("玩家共四位");
+			players = new String[4][6];
+		}
+		for (int i = 0; i < playersSum; i++) {
+			for (int i2 = 0; i2 < players[0].length; i2++) {
+				players[i][i2] = shuffledPoker[i2 + (i * players[0].length)];
+				shuffledPoker[i2 + (i * players[0].length)] = "0";
+			}
+		}
+
+		return players;
+	}
+
+	public static String[] removeEmptyElement(String[] pokerList) {
+		List<String> tempRemoveList = new ArrayList<String>(Arrays.asList(pokerList));
+		for (int i = 0; i < tempRemoveList.size(); i++) {
+			tempRemoveList.remove("0");
+		}
+		pokerList = tempRemoveList.toArray(new String[tempRemoveList.size()]);
+
+		return pokerList;
+	}
+
+	public static String[] addElement(String[] pokerList, String elm) {
+		List<String> tempAddList = new ArrayList<String>(Arrays.asList(pokerList));
+		tempAddList.add(elm);
+		pokerList = tempAddList.toArray(new String[tempAddList.size()]);
+
+		return pokerList;
+	}
+
+	public static void countScore(String countCardOne, String countCardTwo, int player) {
+// 玩家比對成功的牌
+// 海底比對成功的牌
+		int scorePlayerCard = Integer.parseInt(countCardOne.substring(1).trim());
+		int scoreShowedCard = Integer.parseInt(countCardTwo.substring(1).trim());
+		String suitsFromPlayer = countCardOne.substring(0, 1);
+		String suitsFromShowed = countCardTwo.substring(0, 1);
+
+		if ((scorePlayerCard + scoreShowedCard) == 10) {
+// 判斷幾分
+			if (suitsFromPlayer.equals("♥") || suitsFromPlayer.equals("♦")) {
+				if (scorePlayerCard == 1) {
+					score[player] += 20;
+				} else if (scorePlayerCard == 9) {
+					score[player] += 10;
+				} else {
+					score[player] += scorePlayerCard;
+				}
+			}
+			if (suitsFromShowed.equals("♥") || suitsFromShowed.equals("♦")) {
+				if (scoreShowedCard == 1) {
+					score[player] += 20;
+				} else if (scoreShowedCard == 9) {
+					score[player] += 10;
+				} else {
+					score[player] += scoreShowedCard;
+				}
+			}
+			if (countCardOne.trim().equals("♠1") || countCardTwo.trim().equals("♠1")) {
+				score[player] += 30;
+			}
+		} else if (scorePlayerCard > 9 && scoreShowedCard > 9) {
+			if (scorePlayerCard == scoreShowedCard) {
+				if (suitsFromPlayer.equals("♥") || suitsFromPlayer.equals("♦")) {
+					score[player] += 10;
+				}
+				if (suitsFromShowed.equals("♥") || suitsFromShowed.equals("♦")) {
+					score[player] += 10;
+				}
+			}
+		}
 	}
 }
