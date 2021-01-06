9dcc03cd7ef33b978b4ed4794b363e8687f1de01
SayHello123 <sarachang757@gmail.com>
2021-01-06 18:17:26 +0800

    新增 method copyArrayData(String[] arrayOne, String[] arrayTwo, int arrayLength)

--- basic/src/main/java/sara/basic/issue36/SaraStack.java
+++ basic/src/main/java/sara/basic/issue36/SaraStack.java
@@ -12,42 +12,43 @@ public class SaraStack {
 		// array 只儲存舊的
 		String[] newArr = new String[array.length + 1];
 		// newArr 複製 array 的值
-		for (int i = 0; i < array.length; i++) {
-			newArr[i] = array[i];
-		}
+		copyArrayData(newArr, array, array.length);
 		// newArr 再加一個新的
 		newArr[newArr.length - 1] = value;
 		// array 複製 newArr
 		array = new String[newArr.length];
-		for (int i = 0; i < newArr.length; i++) {
-			array[i] = newArr[i];
-		}
+		copyArrayData(array, newArr, newArr.length);
 		top = array.length - 1;
 	}
 
 	public String pop() {
-		if (array.length > 0) {
+		if (array.length <= 0) {
+			return "It's empty 此罐已空";
+		} else {
 			String last = array[top];
 			String[] newArr = new String[array.length - 1];
 			// newArr 複製 array.length-1 的值
-			for (int i = 0; i < newArr.length; i++) {
-				newArr[i] = array[i];
-			}
+			copyArrayData(newArr, array, newArr.length);
 			// array 複製 newArr
 			array = new String[newArr.length];
-			for (int i = 0; i < newArr.length; i++) {
-				array[i] = newArr[i];
-			}
+			copyArrayData(array, newArr, newArr.length);
 			top = array.length - 1;
 			return last;
 		}
-		return "It's empty 此罐已空";
 	}
 
 	public String peek() {
-		if (array.length > 0) {
+		if (array.length <= 0) {
+			return "It's empty 此罐已空";
+		} else {
 			return array[top];
 		}
-		return "It's empty 此罐已空";
+	}
+
+	public void copyArrayData(String[] arrayOne, String[] arrayTwo, int arrayLength) {
+		// arrayOne 複製 newArray 的值
+		for (int i = 0; i < arrayLength; i++) {
+			arrayOne[i] = arrayTwo[i];
+		}
 	}
 }
