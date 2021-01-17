9bc0e4f57e8072e84775f15417e519c5c5dc2177
SayHello123 <sarachang757@gmail.com>
2021-01-17 16:17:17 +0800

    新增 checkSame()、修改 createSquareWithNumber() 的判斷式與邏輯

--- basic/src/main/java/sara/basic/issue16/Main.java
+++ basic/src/main/java/sara/basic/issue16/Main.java
@@ -4,20 +4,25 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	static int squareSize;
+	static int squareSize = 3;
+	static String[] square;
+	static String temp;
+	static int x;
+	static int y;
 
 	public static void main(String[] args) {
 		// 獲得大小
 		getSquareSize();
 		// 將大小給製作方陣的 method 使用
-		int[] cube = createSquareWithNumber();
+		String[] cube = createSquareWithNumber();
 		for (int i = 1; i <= cube.length; i++) {
 			if ((i % squareSize) == 0) {
-				System.out.print(cube[i - 1] + "\n");
+				cube[i - 1] += "\n";
 			} else {
-				System.out.print(cube[i - 1] + "\t");
+				cube[i - 1] += "\t";
 			}
 		}
+		System.out.println(Arrays.toString(cube));
 	}
 
 	public static int getSquareSize() {
@@ -27,20 +32,50 @@ public class Main {
 		return squareSize;
 	}
 
-	public static int[] createSquareWithNumber() {
-		// 只能做出 3 x 3 的方陣
-		int middleNum = (squareSize * 2) - 1;
-		int[] square = new int[squareSize * squareSize];
-		square[square.length / 2] = middleNum;
-		square[(square.length / 2) - 1] = middleNum - 2;
-		square[(square.length / 2) + 1] = middleNum + 2;
-		square[(square.length / 2) - 2] = middleNum + 1;
-		square[(square.length / 2) + 2] = middleNum - 1;
-		square[(square.length / 2) - 3] = middleNum - 4;
-		square[(square.length / 2) + 3] = middleNum + 4;
-		square[(square.length / 2) - 4] = middleNum + 3;
-		square[(square.length / 2) + 4] = middleNum - 3;
+	public static String[] createSquareWithNumber() {
+		square = new String[squareSize * squareSize];
+		// 填入座標值
+		for (int y = 0; y < squareSize; y++) {
+			for (int x = 0; x < squareSize; x++) {
+				square[(squareSize * y) + x] = x + ":" + y;
+			}
+		}
+		System.out.println(Arrays.toString(square));
+
+		// 先把 1 填入 first row 的中間
+		square[squareSize / 2] = "1";
+		x = squareSize / 2;
+		y = 0;
+		for (int i = 2; i <= square.length; i++) {
+			if (i % squareSize == 1) {
+				y++;
+				checkSame(i);
+				System.out.println(i + " con=> " + temp);
+				continue;
+			}
+
+			x--;
+			y--;
+			if (x < 0) {
+				x += squareSize;
+			} else if (y < 0) {
+				y += squareSize;
+			}
+
+			checkSame(i);
+
+			System.out.println(i + " => " + temp);
+		}
 		System.out.println(Arrays.toString(square));
 		return square;
+	}
+
+	public static void checkSame(int currentNum) {
+		temp = x + ":" + y;
+		for (int i = 0; i < square.length; i++) {
+			if (temp.equals(square[i])) {
+				square[i] = currentNum + "";
+			}
+		}
 	}
 }
