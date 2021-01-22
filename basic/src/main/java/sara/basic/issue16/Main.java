dd9625e50e188ced71b28559b0fac0e7ca54d5bc
SayHello123 <sarachang757@gmail.com>
2021-01-22 19:33:53 +0800

    新增 fillInNumber()  填入數字、新增 drawSquare() 的判斷條件

--- basic/src/main/java/sara/basic/issue16/Main.java
+++ basic/src/main/java/sara/basic/issue16/Main.java
@@ -1,13 +1,15 @@
 package sara.basic.issue16;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		int size = getSquareSize();
 		int[] squareShape = createSquareShape(size);
+		int[] finalSquare = fillInNumber(squareShape);
 
-		drawSquare(size, squareShape);
+		drawSquare(size, finalSquare);
 	}
 
 	public static int getSquareSize() {
@@ -21,9 +23,18 @@ public class Main {
 		return squareShape;
 	}
 
+	public static int[] fillInNumber(int[] squareShape) {
+		int[] finalSquare = squareShape;
+		// 先把 1 填入 first row 的中間
+		// 宣告 x, y
+		// 加入填入的規則
+		System.out.println(Arrays.toString(finalSquare));
+		return finalSquare;
+	}
+
 	public static void drawSquare(int size, int[] finalSquare) {
 		for (int i = 0; i < finalSquare.length; i++) {
-			if ((i % size) == 0) {
+			if ((i % size) == 0 && i > 0) {
 				System.out.println();
 			}
 			System.out.print(finalSquare[i] + "\t");
